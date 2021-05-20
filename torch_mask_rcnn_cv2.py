3aeb823d9c7c4535f19192837c28929426765a7a
vincent51689453 <vincent51689453@gmail.com>
2021-05-20 11:39:04 +0800

    opencv for image is ok!

---
+++ torch_mask_rcnn_cv2.py
@@ -0,0 +1,75 @@
+import torchvision
+from PIL import Image
+from torchvision import transforms as T
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import random
+
+# pip3 install torchvision==0.5.0
+# sudo apt-get install python3-matplotlib
+#Checkpoint path: /home/vincent/.cache/torch/checkpoints/maskrcnn_resnet50_fpn_coco-bf2d0c1e.pth
+
+model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+model.eval()
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+def get_prediction(img_path, threshold):
+	img = Image.open(img_path)
+	transform = T.Compose([T.ToTensor()])
+	img = transform(img)
+	model.cuda()
+	img = img.cuda()
+	pred = model([img])
+	pred_score = list(pred[0]['scores'].detach().cpu().numpy())
+	pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1]
+	masks = (pred[0]['masks'] > 0.5).squeeze().detach().cpu().numpy()
+	pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].cpu().numpy())]
+	pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().cpu().numpy())]
+	masks = masks[:pred_t+1]
+	pred_boxes = pred_boxes[:pred_t+1]
+	pred_class = pred_class[:pred_t+1]
+	return masks, pred_boxes, pred_class
+
+def random_colour_masks(image):
+	colours = [[0, 255, 0],[0, 0, 255],[255, 0, 0],[0, 255, 255],
+				[255, 255, 0],[255, 0, 255],[80, 70, 180],
+				[250, 80, 190],[245, 145, 50],[70, 150, 250],
+				[50, 190, 190]]
+	r = np.zeros_like(image).astype(np.uint8)
+	g = np.zeros_like(image).astype(np.uint8)
+	b = np.zeros_like(image).astype(np.uint8)
+	r[image == 1], g[image == 1], b[image == 1] = colours[random.randrange(0, 10)]
+	coloured_mask = np.stack([r, g, b], axis=2)
+	return coloured_mask
+
+def instance_segmentation_api(img_path, threshold=0.5, rect_th=3, text_size=3, text_th=3):
+	masks, boxes, pred_cls = get_prediction(img_path, threshold)
+	img = cv2.imread(img_path)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	for i in range(len(masks)):
+		rgb_mask = random_colour_masks(masks[i])
+		img = cv2.addWeighted(img, 1, rgb_mask, 0.5, 0)
+		cv2.rectangle(img, boxes[i][0], boxes[i][0], color=(0, 255, 0), thickness=rect_th)
+		cv2.putText(img, pred_cls[i], boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0, 255, 0), 		
+			thickness=text_th)
+	cv2.imshow('Output',img)
+
+
+instance_segmentation_api('car.jpg', threshold=0.5, rect_th=6, text_th=1, text_size=1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
